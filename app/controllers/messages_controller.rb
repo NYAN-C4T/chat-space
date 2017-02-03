6c698146a4dd81498f6b0015fbbf4caebb777c30
NYAN-C4T <tesseract13th@outlook.com>
2017-02-03 12:44:18 +0900

    refactor parameter-related things at controller, modify conditions at validation check

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,24 +4,24 @@ class MessagesController < ApplicationController
   def index
   end
 
-  def create
-    prm = params[:message].permit(:body).merge(message_params)
-    msg = Message.create(prm)
-    if msg.valid?
-      redirect_to group_messages_path
-    else
-      flash.now[:alert] = msg.errors.full_messages[0]
-      render :index
-    end
+def create
+  msg = Message.create(body: message_params[:body], group_id: params[:group_id], user_id: current_user.id)
+  if msg.persisted?
+    redirect_to group_messages_path
+  else
+    flash.now[:alert] = msg.errors.full_messages[0]
+    render :index
   end
+end
 
   private
   def message_params
-    params.permit(:group_id).merge(user_id: current_user.id)
+    params.require(:message).permit(:body)
   end
 
   def get_group_messages
     @message = Message.new
-    @messages = Message.includes(:user).where(group_id: 1)
+    @messages = Message.includes(:user).where(group_id: params[:group_id])
   end
 end
+
