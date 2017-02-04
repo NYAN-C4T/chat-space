82d07c4211727814797df9392f4203c7ca173abe
NYAN-C4T <tesseract13th@outlook.com>
2017-02-04 18:54:08 +0900

    modify notation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,14 @@
 class MessagesController < ApplicationController
-  before_action :get_group_messages, only: %i(index create)
+  before_action :get_messages, only: %i(index create)
 
   def index
     @message = Message.new
   end
 
   def create
-    @message = @messages.build(message_params)
+    @message = @messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@message.group_id)
+      redirect_to group_messages_path(params[:group_id])
     else
       flash.now[:alert] = @message.errors.full_messages[0]
       render :index
@@ -20,8 +20,8 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:body).merge(user_id: current_user.id)
   end
 
-  def get_group_messages
-    @messages = Message.where(params[:group_id])
+  def get_messages
+    @messages = Message.where(group_id: params[:group_id])
   end
 end
 
