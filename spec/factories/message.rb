168b194ae05be631ba84847a640552b131d19cd8
NYAN-C4T <tesseract13th@outlook.com>
2017-02-11 21:41:07 +0900

    update gem ver. and refactor specs by using context, test valid message

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -3,8 +3,6 @@ FactoryGirl.define do
   factory :message do
     body       { Faker::Lorem.word }
     image      { Faker::Internet.url }
-    user_id    1
-    group_id   1
     created_at { Faker::Time.between(2.days.ago, Time.now, :all)}
     updated_at { Faker::Time.between(2.days.ago, Time.now, :all)}
   end
