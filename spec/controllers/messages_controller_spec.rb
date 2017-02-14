2cf5d4341a15843f08530620f32707f2566341df
NYAN-C4T <tesseract13th@outlook.com>
2017-02-14 17:10:28 +0900

    test message controller

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -7,4 +7,31 @@ describe MessagesController do
       expect(response).to render_template :index
     end
   end
+
+  describe 'POST #create' do
+    context 'when message has created correctly' do
+      it 'creates a valid record' do
+        user = create(:user)
+        group = create(:group)
+        message = create(:message, user_id: user.id, group_id: group.id)
+        expect(message).to be_valid
+      end
+      it 'renders the :index template' do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'when a message has NOT created' do
+      it 'NO record has created' do
+        message = build(:message)
+        message.valid?
+        expect(message.errors.full_messages[0]).to include('が不正な値です。')
+      end
+      it 'renders the :index template' do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+  end
 end
--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :group do
-    group_name "MyString"
+    group_name { Faker::Lorem.word }
   end
 end
