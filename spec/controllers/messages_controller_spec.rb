246230e4d8f34c2335efe538dd7fde117ff4bb2b
NYAN-C4T <tesseract13th@outlook.com>
2017-02-16 13:56:20 +0900

    add instance value test, checking message (both persisted and not)

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 describe MessagesController do
   describe 'GET #index' do
+    it 'assigns the requested message to @messages' do
+      user = create(:user)
+      group = create(:group)
+      message = create(:message, user_id: user.id, group_id: group.id)
+      get :index, group_id: group.id
+      messages = Message.where(group_id: group.id)
+      expect(assigns(:messages)).to eq messages
+    end
+    it 'assigns the requested message to @message' do
+      get :index
+      expect(assigns(:message).new_record?).to be true
+    end
     it 'renders the :index template' do
       get :index
       expect(response).to render_template :index
@@ -14,7 +26,7 @@ describe MessagesController do
         user = create(:user)
         group = create(:group)
         message = create(:message, user_id: user.id, group_id: group.id)
-        expect(message).to be_valid
+        expect(message.persisted?).to be true
       end
       it 'renders the :index template' do
         get :index
@@ -25,8 +37,7 @@ describe MessagesController do
     context 'when a message has NOT created' do
       it 'NO record has created' do
         message = build(:message)
-        message.valid?
-        expect(message.errors.full_messages[0]).to include('が不正な値です。')
+        expect(message.save).to be false
       end
       it 'renders the :index template' do
         get :index
